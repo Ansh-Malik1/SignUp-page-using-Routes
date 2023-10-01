11c7d76ad51546c0fa68cffacd943b82d36458cf
Ansh-Malik1 <anshmalik718@gmail.com>
2023-10-01 17:49:08 +0530

    pages

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <div className="flex flex-1 justify-center items-center  w-screen h-screen text-white font-bold text-4xl ">
+      <h1 className=" mx-auto text-center">Welcome to Studynotion!</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex flex-1 justify-center items-center  w-screen h-screen text-white font-bold ">
+      <h1 className="text-5xl ">Home</h1>
+    </div>
+  );
+}
+
+export default Home;
